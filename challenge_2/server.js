c1876d0340673a10df8893b089c258367b81bb8b
Linden Chiu <hchiu87@gmail.com>
2018-12-11 11:55:17 -0800

    create server and serve static assets

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, './client')));
+
+
+
+
+
+
+
+
+const PORT = 3000; 
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
