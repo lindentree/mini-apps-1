c935dccc2f094abcabe08a9f829cd5f6a06cf6e9
Linden Chiu <hchiu87@gmail.com>
2018-12-12 14:12:15 -0800

    serve all static assets check

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,16 @@
+const path = require('path');
+const express = require ('express');
+const app = express();
+
+app.use(express.static('public'));
 
 
+
+app.get('/public', (req, res) => {
+	console.log('Hello');
+	res.end();
+})
+
+const PORT = 3003;
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`))
