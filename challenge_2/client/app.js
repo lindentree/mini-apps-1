dde76c19f89b27b21d6e4c1aa9458d9980852154
Linden Chiu <hchiu87@gmail.com>
2018-12-11 16:33:56 -0800

    implement form field

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,3 +1,9 @@
 
+var userInput = document.getElementById('#input');
 
+var button = document.getElementById('#submit');
 
+//implement fetch post function, submit onclick
+//fetch()
+
+//JSON to CSV parsing help function
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, './client')));
 
+app.get('/csv', (req, res) =>{
+  res.redirect('/');
+  console.log(req.query.data)
+  res.statusCode = 200;
+  res.end();
+});
 
+app.post('/csv', (req, res) =>{
+  res.statusCode = 201;
+  res.redirect('/');
+  console.log(req.query.data)
+  //res.send('working');
+  res.end();
+});
 
 
 
