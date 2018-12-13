12d424b3973e2b4304df13966239eb77b02bc4c4
Linden Chiu <hchiu87@gmail.com>
2018-12-13 12:33:53 -0800

    implement React component board

--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -1,16 +1,98 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class ConnectFourRow extends React.Component {
-   render() {
-   	 return (
-       <div>
-         <h1> Hello world! </h1>
-       </div>
+// class ConnectFourRow extends React.Component {
+//    render() {
+//    	 return (
+//        <div>
+//          <h1> Hello world! </h1>
+//        </div>
 
-   	 	);
-   }
+//    	 	);
+//    }
 
+// }
+
+
+class Board extends React.Component {
+  renderSquare(val) {
+    return <Square value={val} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+          {this.renderSquare(null)}
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<ConnectFourRow />, document.getElementById('app'));
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+
+ReactDOM.render(<Board />, document.getElementById('app'));
