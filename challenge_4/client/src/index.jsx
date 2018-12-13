97696568860d0a69a086eb3a1a5a531b00805baf
Linden Chiu <hchiu87@gmail.com>
2018-12-13 12:16:23 -0800

    server serves html with hello world

---
+++ challenge_4/client/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class ConnectFourRow extends React.Component {
+   render() {
+   	 return (
+       <div>
+         <h1> Hello world! </h1>
+       </div>
+
+   	 	);
+   }
+
+}
+
+ReactDOM.render(<ConnectFourRow />, document.getElementById('app'));
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 
+const PORT = 3004;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('./public'));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public/index.html'));
+// });
+//app.get('/', (req, res) => res.send('Hello World'));
+app.listen(PORT, () => console.log(`Connect4 app listening on port ${PORT}!`));
 
